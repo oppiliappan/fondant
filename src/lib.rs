ca0dc4be5174481b0fe3ce0483df00fe44b55b51
Akshay <nerdypepper@tuta.io>
2020-03-19 17:13:19 +0530

    add expand_tilde util fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-pub use directories::ProjectDirs;
+pub use directories::{ProjectDirs, UserDirs};
 pub use serde::{de::DeserializeOwned, Serialize};
 pub use serde_json;
 pub use serde_yaml;
 pub use toml;
+
+use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
 pub enum FondantError {
@@ -13,6 +15,20 @@ pub enum FondantError {
     FileWriteError,
     FileReadError,
     FileOpenError,
+}
+
+pub fn expand_tilde<P: AsRef<Path>>(path: P) -> PathBuf {
+    let p = path.as_ref();
+    if p.starts_with("~") {
+        if p == Path::new("~") {
+            return UserDirs::new().unwrap().home_dir().to_path_buf();
+        } else {
+            let mut h = UserDirs::new().unwrap().home_dir().to_path_buf();
+            h.push(p.strip_prefix("~/").unwrap());
+            return h;
+        }
+    }
+    return p.to_path_buf();
 }
 
 pub trait Configure: Serialize + DeserializeOwned + Default {
